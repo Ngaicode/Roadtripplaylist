53ea1d4c3a308c58100f9b68c145b639d33bcfb4
Ngaicode <Mjombakevin376@gmail.com>
2022-01-05 14:54:20 +0300

    add comments on code

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ console.log(questionPrompt("Kevin"));
 
 
 
+
+
+// In the case of both function expresion and arrow functions,the function names are anonymous hence these type of functions can be assigned to a variable .To call such functions ,one would use the variable name assigned to it followed by parenthesis then parameters(if any)
+
 // function expression
 const expresion = function (jina) {
     const swali = `${jina},  Have you eaten ?`;
@@ -23,6 +27,7 @@ console.log(expresion("Mjomba"));
 
 
 // arrow functions
+// arrow functions are a different kind of function expressions
 const arrow = nem => {
     const swali = `${nem},  Have you eaten ?`;
     return swali;
